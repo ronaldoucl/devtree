fc80a9fb014bda1233808117c8759b78ba195b22
ronaldoucl <ronaldocl9lda@gmail.com>
2026-05-18 00:10:45 -0600

    login and register views and register form

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,22 +3,13 @@ import {Link} from "react-router-dom";
 export default function LoginView() {
     return (
         <>
-            <div className="bg-slate-800 min-h-screen">
-                <div className="max-w-lg mx-auto pt-10 px-5">
-                    <img src="/logo.svg" alt="Logotipo DevTree" />
-                    <div className="py-10">
-                        <h1 className="text-white">Login</h1>
-                    </div>
-                </div>
-                
-            </div>
-
-            <nav className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4">
-                <div className="container mx-auto flex justify-center space-x-4">
-                    <Link to="/auth/register" className="px-4 py-2 bg-green-500 rounded hover:bg-green-600 transition">
-                        ¿No tienes una cuenta? Regístrate
-                    </Link>
-                </div>
+            <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+            <nav className="mt-10">
+                <Link
+                    to="/auth/register"
+                    className="text-white text-center text-lg block">
+                    ¿No tienes una cuenta? Regístrate
+                </Link>
             </nav>
         </>
     );
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,18 +1,102 @@
 
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../components/ErrorMessage";
+
 
 export default function RegisterView() {
+    const initialValues = {
+        name: '',
+        email: '',
+        handle: '',
+        password: '',
+        password_confirmation: '',
+    }
+
+    const { register, watch, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues });
+
+    const handleRegister = () => {
+        console.log("Desde handleRegister");
+    }
+
     return (
         <>
-            <div className="flex items-center justify-center h-screen">
-                <h1 className="text-4xl font-bold">Register View</h1>
-            </div>
-            <nav className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4">
-                <div className="container mx-auto flex justify-center space-x-4">
-                    <Link to="/auth/login" className="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600 transition">
-                        ¿Ya tienes una cuenta? Inicia sesión
-                    </Link>
+            <h1 className="text-4xl text-white font-bold">Crear Cuenta</h1>
+
+            <form
+                onSubmit={handleSubmit(handleRegister)}
+                className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+            >
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name', { required: 'El nombre es requerido' })}
+                    />
+                    {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage> }
                 </div>
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Email de Registro"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email', { required: 'El email es requerido' })}
+                    />
+                    {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage> }
+                </div>
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
+                    <input
+                        id="handle"
+                        type="text"
+                        placeholder="Nombre de usuario: sin espacios"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('handle', { required: 'El handle es requerido' })}
+                    />
+                    {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage> }
+                </div>
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password de Registro"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', { required: 'El password es requerido' })}
+                    />
+                    {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage> }
+                </div>
+
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
+                    <input
+                        id="password_confirmation"
+                        type="password"
+                        placeholder="Repetir Password"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password_confirmation', { required: 'La confirmación del password es requerida' })}
+                    />
+                    {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage> }
+                </div>
+
+                <input
+                    type="submit"
+                    className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                    value='Crear Cuenta'
+                />
+            </form>
+
+            <nav className="mt-10">
+                <Link
+                    to="/auth/login"
+                    className="text-white text-center text-lg block">
+                    ¿Ya tienes una cuenta? Inicia sesión
+                </Link>
             </nav>
         </>
     );
